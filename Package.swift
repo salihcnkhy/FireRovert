5efb7a59505038d49d3e405c47e3f3acea6e761f
Salihcan Kahya <salihcankahya@gmail.com>
2021-02-15 20:52:19 +0300

    Firebase package added

--- Package.swift
+++ Package.swift
@@ -14,13 +14,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Firebase",
+                 url: "https://github.com/firebase/firebase-ios-sdk.git",
+                 .branch("7.0-spm-beta"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FireRovert",
-            dependencies: []),
+            dependencies: [
+                .product(name: "FirebaseFirestore", package: "Firebase"),
+]),
         .testTarget(
             name: "FireRovertTests",
             dependencies: ["FireRovert"]),
